ffdb307a00b608d3d400ce06871aa94409ff31e1
FatimaZahraFadel <147441122+FatimaZahraFadel@users.noreply.github.com>
2025-01-06 16:50:05 +0100

    Update main.go

---
+++ in-class-project/day 1/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	secretNumber := r.Intn(100) + 1
+	var guess int
+	attempts := 0
+
+	fmt.Println("Guess the number between 1 and 100.")
+
+	for {
+		fmt.Print("Enter your guess: ")
+		fmt.Scan(&guess)
+
+		attempts++
+
+		if guess < secretNumber {
+			fmt.Println("too low")
+		} else if guess > secretNumber {
+			fmt.Println("too high")
+		} else {
+			fmt.Printf("correct You guessed the number in %d attempts.\n", attempts)
+			break
+		}
+	}
+}
